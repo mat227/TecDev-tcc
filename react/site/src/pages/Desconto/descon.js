88706b95c10f274e4b9b05bc6fa22d5980034a4a
EliasCavalcant <ra46772353846@acaonsfatima.org.br>
2021-09-29 14:12:23 -0300

    ok

---
+++ react/site/src/pages/Desconto/descon.js
@@ -0,0 +1,296 @@
+import { ContainerDesc } from "./descon.styled"
+
+
+export default function Homepage() {
+    return (
+    <ContainerDesc>
+
+
+        <div class="conteiner-homepage1">
+
+
+        <div class="hp1-buscap">
+        <a href="../HomePage1/homepage1.html"> <div class="hp1-logo">
+                <img src="/assets/images/nice library png 1.svg" alt="" />
+                <img class="hp1-nome" src="/assets/images/Bookly.svg"  alt="" />
+
+            </div>
+        </a> 
+
+            <div class="hp1-busca">
+                <div class="divBusca">
+                    <input type="text" id="txtBusca" placeholder="Buscar..." />
+                    <button><img src="/assets/images/lupa.svg" id="btnBusca" alt="Buscar" /></button>
+                </div>
+            </div>
+
+            <div class="hp1-seus">
+                <div class="hp1-bolsa"><img src="/assets/images/sua bolsa.svg" alt="" /></div>
+                <div class="hp1-favorito"><img src="/assets/images/seus favoritos.svg"  alt="" /></div>
+            <a href="../Perfil/index.html">   <div class="hp1-perfil"><img src="/assets/images/perfil.svg"  alt="" /></div> </a>
+
+            </div>
+        </div>
+
+
+
+
+        <div class="oferta-da-semana">
+            <div class="trio">
+                <button><img src="/assets/images/triste-feliz.svg" id="btnBusca" alt="Buscar" /></button>
+                <button><img src="/assets/images/caracoracao.svg" id="btnBusca" alt="Buscar" /></button>
+                <button><img src="/assets/images/desconto.svg" id="btnBusca" alt="Buscar" /></button>
+
+
+            </div>
+            <div class="trio2">
+                <div class="titilo-trio1">GÊNEROS</div>
+                <div class="titilo-trio2">PROMOÇÕES</div>
+                <div class="titilo-trio3">CUPONS</div>
+            </div>
+
+            <div class="desconto">
+                <div class="tldescontoori">
+                    <div class="tldesconto1">
+                        Seleção de livros com </br> até 50% de desconto
+                    </div>
+                    <div class="timer">
+                    </div>
+                </div>
+
+                <div class="livros-desconto">
+                <a href="../DescricaoLivro/descricao.html"> <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                            <img class="descontolivro"src="/assets/images/Frame 1.svg" alt="" />
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                                De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                                </div>
+
+                        </div>
+                    </div>
+                </a>
+                <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                            <img class="descontolivro"src="/assets/images/Frame 1.svg" alt="" />
+
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                                De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                                </div>
+
+                        </div>
+                    </div>
+                </a>
+                <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="../HomePage1/assets/images/vermelho, branco e sangue azul.svg" />
+                            <img class="descontolivro"src="../HomePage1/assets/images/Frame 1.svg"/>
+
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                            De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+
+                </div>
+                <div class="livros-desconto2">
+                <a href="../DescricaoLivro/descricao.html"> <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                            <img class="descontolivro"src="/assets/images/Frame 1.svg" alt="" />
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                                De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                                </div>
+
+                        </div>
+                    </div>
+                </a>
+                    <a href="../DescricaoLivro/descricao.html"> <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg"  alt="" />
+                            <img class="descontolivro"src="../HomePage1/assets/images/Frame 1.svg"/>
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                                De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                                </div>
+
+                        </div>
+                    </div>
+                </a>
+                <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                            <img class="descontolivro"src="/assets/images/Frame 1.svg" alt="" />
+
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                            De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+                </div>
+                <div class="livros-desconto2">
+                    <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                        <div class="img-livro-queridinha">
+                            <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                            <img class="descontolivro"src="/assets/images/Frame 1.svg" alt="" />
+
+                        </div>
+                        <div class="box-informacoes-livros">
+                            <div class="nome-livro-desconto">
+                                VERMELHO, BRANCO E SANGUE AZUL
+
+                            </div>
+                            <div class="autor-desconto">
+                                Casey Mcquiston
+                            </div>
+                            <div class="preco-descontotr">
+                            De:R$ 49,90
+                            </div>
+                            <div class="preco-desconto">
+                                Por:R$ 30,90
+                            </div>
+
+                        </div>
+                    </div>
+                </a>
+                <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                    <div class="img-livro-queridinha">
+                        <img src="/assets/images/vermelho, branco e sangue azul.svg" alt="" />
+                        <img class="descontolivro"src="/assets/images/Frame 1.svg" alt=""/>
+
+                    </div>
+                    <div class="box-informacoes-livros">
+                        <div class="nome-livro-desconto">
+                            VERMELHO, BRANCO E SANGUE AZUL
+
+                        </div>
+                        <div class="autor-desconto">
+                            Casey Mcquiston
+                        </div>
+                        <div class="preco-descontotr">
+                        De:R$ 49,90
+                        </div>
+                        <div class="preco-desconto">
+                            Por:R$ 30,90
+                        </div>
+
+                    </div>
+                </div>
+            </a>
+            <a href="../DescricaoLivro/descricao.html">  <div class="box-livro">
+                <div class="img-livro-queridinha">
+                    <img src="../HomePage1/assets/images/vermelho, branco e sangue azul.svg" />
+                    <img class="descontolivro"src="../HomePage1/assets/images/Frame 1.svg"/>
+
+                </div>
+                <div class="box-informacoes-livros">
+                    <div class="nome-livro-desconto">
+                        VERMELHO, BRANCO E SANGUE AZUL
+
+                    </div>
+                    <div class="autor-desconto">
+                        Casey Mcquiston
+                    </div>
+                    <div class="preco-descontotr">
+                    De:R$ 49,90
+                    </div>
+                    <div class="preco-desconto">
+                        Por:R$ 30,90
+                    </div>
+
+                </div>
+            </div>
+        </a>
+                </div>
+            
+                </div>
+                
+            
+        </div>
+        <footer>
+            <div class="logowo"><img src="/assets/images/logo atual.svg" alt="" />by: TecDev</div>
+            <div class="redes"><img src="/assets/images/insta.svg" alt="" /><img
+                    src="../DescricaoLivro/assets/images/facebook.svg" /></div>
+        </footer>
+
+
+
+    </ContainerDesc>
+    )
+}
