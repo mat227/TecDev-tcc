cc628c1383987a72f6f71a36007e04d5d20ebcc9
nicocchina <ra41105586898@acaonsfatima.org.br>
2021-10-01 17:17:27 -0300

    matheus

--- react/site/src/pages/genero/genp.conteudo.js
+++ react/site/src/pages/genero/genp.conteudo.js
@@ -3,7 +3,21 @@ import { ContainerGenP } from './genp.styled'
 export default function GenPConteudo() {
     return (
         <ContainerGenP>
+                <div class="titulo">
+        <div class="quadrado" style="margin-right: 0.5em;"></div>
+            <p>GÊNEROS</p>
+        <div class="quadrado" style="margin-left: 0.5em;"></div>
+    </div>
 
+    <div class="generos">
+
+        <div className="itens"><link to="../pages/horror.html" class=""><img src="../../assets/images/TERROR.svg" alt=""/></link></div>
+        <div className="itens"><link href="../pages/humor.html" class=""><img src="../../assets/images/HUMOR.svg" alt=""></link></div>
+        <div className="itens"><link href="../pages/romance.html" class=""><img src="../../assets/images/ROMANCE.svg" alt=""/></link></div>
+        <div className="itens"><a href="../pages/biografia.html" class=""><img src="../../assets/images/BIOGRAFIA.svg" alt=""/></a></div>
+        <div className="itens"><link href="../pages/poesia.html" class=""><img src="../../assets/images/POESIA.svg" alt=""/></link></div>
+
+    </div>
         </ContainerGenP>
     )
 };
