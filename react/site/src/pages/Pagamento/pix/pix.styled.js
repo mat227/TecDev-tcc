0c97882f0eddae330d4ab733da37acfca97b5d1d
nicocchina <ra41105586898@acaonsfatima.org.br>
2021-10-04 13:23:27 -0300

    nico done pagamentos credito

--- react/site/src/pages/Pagamento/pix/pix.styled.js
+++ react/site/src/pages/Pagamento/pix/pix.styled.js
@@ -3,16 +3,7 @@ import styled from "styled-components";
 const ContainerPix = styled.div `
 
 
-@font-face {
-        font-family: "Rowdies";
-        src: url('../RedefinirSenha/assets/fonts/Rowdies-Bold.ttf');
-    }
-
-@font-face {
-    font-family: "Quicksand";
-    src: url('../RedefinirSenha/assets/fonts/Quicksand-VariableFont_wght.ttf');
-}
-
+c
 `
 
 export { ContainerPix }
