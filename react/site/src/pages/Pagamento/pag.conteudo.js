027e408ad1bc617db35904bbd8fc8956598284b7
nicocchina <ra41105586898@acaonsfatima.org.br>
2021-10-04 01:36:29 -0300

    nico

---
+++ react/site/src/pages/Pagamento/pag.conteudo.js
@@ -0,0 +1,13 @@
+import ParteCima from "../../components/parteCima/componente";
+import Rodape from "../../components/rodape/redape";
+import { ContainerPag } from "./pag.styled";
+
+export default function Pagamentos() {
+    return (    
+        <ContainerPag>
+            <ParteCima/>
+                    <h1></h1>
+            <Rodape />
+        </ContainerPag>
+    );
+}
---
+++ react/site/src/pages/Pagamento/pag.styled.js
@@ -0,0 +1,6 @@
+import styled  from "styled-components";
+
+
+const ContainerPag = styled.div`
+`
+export {ContainerPag}
