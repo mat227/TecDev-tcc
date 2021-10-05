803b886972ff49f05e96d35169954e87ae0bc2de
nicocchina <ra41105586898@acaonsfatima.org.br>
2021-10-05 13:05:53 -0300

    nico done seuspedidos

--- react/site/src/pages/SeusPedidos/pedi.conteudo.js
+++ react/site/src/pages/SeusPedidos/pedi.conteudo.js
@@ -7,8 +7,41 @@ export default function SeusPedidos() {
     return (   
         <ContainerPedido>
             <ParteCima/>
-            
-            
+            <div className="conteudo">
+                <div className="titulo"><h1>SEUS PEDIDOS</h1></div>
+                <div className="box">
+                    <div className="item">
+                        <div className="livro"><img src="/assets/images/o que o sol faz com as florescomprar.svg" alt=""/></div>
+                        <div className="descricao">O QUE O SOL FAZ COM AS FLORES</div>
+                        <div className="menu">
+                            <div className="menu-item">
+                                <div className="imagem"><img src="/assets/images/caminhaoicone.svg" alt="" style={{marginLeft: 0.3 + "em"}}/></div>
+                                <div className="descricao">Transportadora<br/><div style={{fontSize: 0.8 + "em", color:"#6ECAEF"}}>28/08/2021 10:15:23</div></div>
+                            </div>
+                            <div className="quadrado" style={{marginLeft: -2 + "em", width: 5 + "em",marginRight: -2 + "em"}}></div>
+                            <div className="menu-item">
+                                <div className="imagem"><img src="/assets/images/transferencia.svg" alt="" style={{marginLeft: 0.3 + "em"}}/></div>
+                                <div className="descricao" style={{fontSize: 0.9 + "em", lineHeight:1.3+"em"}}>Em Transferência<br/><div style={{fontSize: 0.9 + "em", color:"#6ECAEF"}}>28/08/2021 12:51:36</div></div>
+                            </div>
+                            <div className="quadrado" style={{marginLeft: -2 + "em", width: 5 + "em",marginRight: -2 + "em"}}></div>
+                            <div className="menu-item">
+                                <div className="imagem"><img src="/assets/images/pedidorecebido.svg" alt="" style={{marginLeft: 0.3 + "em"}}/></div>
+                                <div className="descricao">Pedido Recebido<br/><div style={{fontSize: 0.8 + "em", color:"#6ECAEF"}}>Aguardando...</div></div>
+                            </div>
+                            <div className="quadrado" style={{marginLeft: -2 + "em", width: 5 + "em",marginRight: -2 + "em"}}></div>
+                            <div className="menu-item">
+                                <div className="imagem"><img src="/assets/images/saiupraentrega.svg" alt="" style={{marginLeft: 0.3 + "em"}}/></div>
+                                <div className="descricao" style={{fontSize: 0.9 + "em", lineHeight:1.3+"em"}}>Saiu para Entrega<br/><div style={{fontSize: 0.9 + "em", color:"#6ECAEF"}}>Aguardando...</div></div>
+                            </div>
+                            <div className="quadrado" style={{marginLeft: -2 + "em", width: 5 + "em",marginRight: -2 + "em"}}></div>
+                            <div className="menu-item">
+                                <div className="imagem"><img src="/assets/images/entregaconcluida.svg" alt="" style={{marginLeft: 0.3 + "em"}}/></div>
+                                <div className="descricao"  style={{fontSize: 0.9 + "em", lineHeight:1.3+"em"}}>Entrega Concluída<br/><div style={{fontSize: 0.9 + "em", color:"#6ECAEF"}}>Aguardando...</div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Rodape/>
         </ContainerPedido>
      );
--- react/site/src/pages/SeusPedidos/pedi.styled.js
+++ react/site/src/pages/SeusPedidos/pedi.styled.js
@@ -11,6 +11,72 @@ height: 100%;
 background-color: #135578;
 background-image: url('/assets/images/bacckground.svg');
 
+font-family: "Rowdies";
+
+    .conteudo{
+        background-color:  rgba(18, 23, 43, 0.5);
+        border-radius: 2em;
+        height: 85vh;
+        margin: 1em;
+        width: 80vw;
+        padding: 2em 3em;
+        
+        display:flex;
+        flex-direction: column;
+
+        margin: 1em auto;
+
+        color: white;
+    }
+
+    .titulo {
+        flex-grow: 1;
+        font-family: Rowdies;
+        margin-top: -2em;
+    }
+    h1 {
+        font-size:2.3em;
+    }
+
+    .box {
+        background: rgba(26, 82, 109, 1);
+        width: 80vw;
+        height: 80vh;
+        border-radius: 15px;
+        padding: 1em;
+    }
+    .item {
+        background: rgba(16, 43, 70, 0.6);
+        margin: 0.8em;
+        padding: 1em 3em;
+        border-radius: 15px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .descricao {
+        margin-left: 0.5em;
+        width: 10vw;
+        font-size: 1em;
+    }   
+
+    .menu-item {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .menu {
+        display: flex;
+    }
+
+    .quadrado {
+        width: 30px;
+        height: 2px;
+        background: #6ECAEF;
+        margin-top: 2.8em;
+    }
 
 `
 export {ContainerPedido}
