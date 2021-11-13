6f859d33ad480f9902de8bdf945c0bc642fbbccd
NicolyAlves <ra41105586898@acaonsfatima.org.br>
2021-11-13 11:24:31 -0300

    nico

--- react/site/src/components/Common/partecima2/cima.js
+++ react/site/src/components/Common/partecima2/cima.js
@@ -2,9 +2,30 @@
 import React, { useState, useEffect } from "react";
 import { CSSTransition } from "react-transition-group";
 import { Responsivo } from "./styled";
+import { useHistory } from 'react-router'
+import { Link } from 'react-router-dom'
 
 
-export default function Header() {
+export default function Header(props) {
+  const [search, setSearch] = useState();
+  const navig = useHistory();
+
+  function keyPress(event) {
+
+    if(event.charCode === 13) {
+        navig.push(`/busca?search=${search}`);
+    }
+  }
+
+
+
+
+
+
+
+
+
+  /// meu
   const [isNavVisible, setNavVisibility] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
 
@@ -33,18 +54,41 @@ export default function Header() {
   return (
       <Responsivo>
         <header className="Header">
-        <img src='/assets/images/logoa.png' className="Logo" alt="logo" />
+          <div className="logoo">
+            <img src="/assets/images/nice library png 1.svg" className="Logo" alt="logo" />
+            <img src="/assets/images/Bookly.svg" className="Logo" alt="logo" />
+          </div>
         <CSSTransition
             in={!isSmallScreen || isNavVisible}
             timeout={350}
             classNames="NavAnimation"
             unmountOnExit
         >
+            
+
+
             <nav className="Nav">
-            <a href="/">Home</a>
-            <a href="/">Articles</a>
-            <a href="/">About</a>
-            <button>Logout</button>
+              <Link to="/login">
+                <div className="menu-item">
+                  <img src="/assets/images/bag.svg" alt="" />
+                  Sua Sacola
+                </div>
+              </Link>
+              <Link to="/login">
+                <div className="menu-item">
+                <img src="/assets/images/coracaoo.svg" alt="" />
+                  Seus Favoritos
+                </div>
+              </Link>
+              <Link to="/login">
+                <div className="menu-item">
+                  <img src="/assets/images/profilee (1).svg" alt=""  />
+                  Login
+                </div>
+              </Link>
+              <Link to="/adm_livro">
+                <button>ADM</button>
+              </Link> 
             </nav>
         </CSSTransition>
         <button onClick={toggleNav} className="Burger">
--- react/site/src/components/Common/partecima2/styled.js
+++ react/site/src/components/Common/partecima2/styled.js
@@ -4,22 +4,44 @@ import styled from 'styled-components'
 const Responsivo = styled.div`
 
 .Header {
-    position: fixed;
     top: 0; /* Stick it to the top */
-    max-height: 70px;
-    width: 100vw;
+    max-height: 12vh;
+    width: 100%;
 
     display: grid;
     grid-template-areas: "logo nav";
 
     /* Cosmetics */
-    background-color: #282c34;
+    background-color: #12172B;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+
+    
+  }
+  
+
+  .menu-item img {
+    height: 2em;
+  }
+
+  .menu-item p{
+    justify-content: space-evenly;
+  }
+
+  .menu-item {
+    display: flex;
+    flex-direction: column !important;
+    
+    align-items: center;
+
+    font-size: 0.8em;
+    font-family: "Quicksand";
   }
 
   .Logo {
     grid-area: logo;
-    height: 70px;
+    height: 2em;
+
+    margin: 1em 0.5em;
   }
 
   .Nav {
@@ -28,6 +50,7 @@ const Responsivo = styled.div`
     grid-template-columns: repeat(4, auto);
     align-items: center;
     justify-items: center;
+    
   }
   .Nav a {
     color: #fff;
@@ -74,17 +97,102 @@ const Responsivo = styled.div`
     transform: scale(1.2);
   }
 
+    .hp1-buscap{
+        display: flex;
+        flex-direction: row;
+        background-color: #12172B;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .logoo {
+        display: flex;
+      }
+
+        
+    .divBusca input{
+      border-top: 0px solid;
+      border-left: 0px solid;
+      border-right: 0px solid;
+      border-bottom: 0px solid;
+      outline: none;
+      color:white;
+    }
+
+    #txtBusca::placeholder{
+        color: white;
+        opacity: 50%;
+    }
+   
+    .divBusca{
+      display: flex;
+      flex-direction: row;
+        background-color:#84D1DD;
+        border:solid 1px;
+        border-radius:10px;
+        width:500px;
+        font-family: "Rowdies";
+        justify-content: space-between;
+      }
+      
+      .divBusca button{
+        margin-top: 0.2em;
+        border: none;
+        background-color: #84D1DD;
+        border-radius: 20px;
+        cursor: pointer;
+}
+
+     
+      #txtBusca{
+        float:left;
+        background-color:transparent;
+        padding-left:10px;
+        font-style:italic;
+        font-size:14px;
+        border:none;
+        height:32px;
+        font-family: "Rowdies";
+      font-weight: 400;
+      width: 35.5em;
+      }
+
+
+      .menu-item {
+        display: flex;
+        flex-direction: column;
+
+        text-align: center;
+
+        margin: 1em 0.5em;
+      }
+
+      
+
+      .descricao {
+        font-family: "Quicksand";
+        color: white;
+        font-size: 0.8em;
+        margin-top: -1em;
+      }
+
+
+      a:Link, a:visited {
+        text-decoration: none;
+      }
+
   @media (max-width: 1024px) {
     .Header {
       grid-template-areas: "logo burger" "nav nav";
     }
     .Nav {
-      grid-template-rows: repeat(4, auto);
-      grid-template-columns: none;
-      grid-row-gap: 20px;
 
-      padding: 30px 0 30px;
-      background: rgba(40, 44, 47, 0.95);
+      grid-template-rows: repeat(4, 2.5em);
+      grid-template-columns: none;
+      grid-row-gap: 1em;
+
+      padding: 2em 0 1em; 
+      background: #12172B;  
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
       border-bottom-left-radius: 10px;
       border-bottom-right-radius: 10px;
@@ -92,7 +200,7 @@ const Responsivo = styled.div`
     .Burger {
       display: inline;
     }
-  }
+  
 
   .NavAnimation-enter {
     opacity: 0;
@@ -101,7 +209,7 @@ const Responsivo = styled.div`
   .NavAnimation-enter-active {
     opacity: 1;
     transform: translateX(0);
-    transition: opacity 350ms, transform 350ms;
+    transition: opacity 15ms, transform 10ms;
   }
   .NavAnimation-exit {
     opacity: 1;
@@ -109,9 +217,21 @@ const Responsivo = styled.div`
   .NavAnimation-exit-active {
     opacity: 0;
     transform: scale(0.5);
-    transition: opacity 350ms, transform 350ms;
+    transition: opacity 15ms, transform 10ms;
   }
 
+  .menu-item img {
+    display: none;
+  }
 
+  .menu-item p {
+    
+  }
+
+  .menu-item {
+    margin-top: 1em;
+  }
+}
+ 
 `
  export { Responsivo }
