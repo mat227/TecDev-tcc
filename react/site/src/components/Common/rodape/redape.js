b23b78426d7fdd47f343b0353bf5c9b91e95fcbd
bia <beatrizfeliciano992@gmail.com>
2021-11-22 00:49:42 -0300

    mds

--- react/site/src/components/Common/rodape/redape.js
+++ react/site/src/components/Common/rodape/redape.js
@@ -4,8 +4,20 @@ export default function Rodape() {
     return (
         <ContainerRodape>
             <div className="roda-mae">
-                <img src="/assets/images/logologo.svg" alt=""/>
-                <div className="ro-logonm">by: TecDev</div>
+                <div className="logo-g">
+                    <div className="logo">
+                        <img src="/assets/images/nice library png 1.svg" alt=""/>
+                        <img class="hp1-nome" src="/assets/images/Bookly.svg" alt=""/>
+                    </div>
+                    <div className="ro-logonm">by: TecDev</div>
+                </div>
+
+                <div className="redes-sociais">
+                    <div className="redes">
+                        <img src="/assets/images/insta.svg"/>
+                        <img src="/assets/images/facebook.svg"/>
+                    </div>
+                </div>
             </div>
         </ContainerRodape>
     )
--- react/site/src/components/Common/rodape/styled.js
+++ react/site/src/components/Common/rodape/styled.js
@@ -7,12 +7,31 @@ background-color: #12172B;
 
 .roda-mae {
   padding: 1em;
-  flex-direction: column !important;
+  display: flex;
+  flex-direction: row !important;
   margin-right: 20em;
+}
+
+.logo-g{
+  diplay:flex;
+  flex-direction: colunm;
 }
 
 img {
   height: 2em;
+  margin-left: 1em;
+
+}
+
+.ro-logonm{
+  color: #9FD4EF;
+  font-family: "Roman";
+  margin-left: 1em;
+}
+
+.redes{
+  display: flex;
+  flex-direction: row;
 }
 
 `
