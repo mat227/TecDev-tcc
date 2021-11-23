de881d48d47b7591282bcfcb263a5bed15f475a1
Matheus <heroimatheuslima2020@hotmail.com>
2021-11-23 00:05:19 -0300

    okoko

--- react/site/src/service/apiLogin.js
+++ react/site/src/service/apiLogin.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create ({
-    baseURL: 'http://localhost:3030/login'
-   // baseURL: 'https://tecdevtcc.herokuapp.com/login'
+    //baseURL: 'http://localhost:3030/login'
+    baseURL: 'https://tecdevtcc.herokuapp.com/login'
 })
 
 export default class Api {
