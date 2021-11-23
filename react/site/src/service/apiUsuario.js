61d1892b586796091e3e65b54f21881e36607167
Thiago Caribe <thiagocaribe01@gmail.com>
2021-11-23 14:43:29 -0300

    ok

--- react/site/src/service/apiUsuario.js
+++ react/site/src/service/apiUsuario.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create ({
-    baseURL : 'http://localhost:3030/usuario',
-   // baseURL: 'https://tecdevtcc.herokuapp.com/usuario'
+    //baseURL : 'http://localhost:3030/usuario',
+    baseURL: 'https://tecdevtcc.herokuapp.com/usuario'
 })
 
 
